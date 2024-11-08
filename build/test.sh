ee655e5eb6c53f44675ca1450609424a01bd0af6
Luis Galeas <galeaspablo@hotmail.com>
2024-11-08 18:44:55 +0000

    Amend test script for expected output

--- build/test.sh
+++ build/test.sh
@@ -6,7 +6,7 @@ echo "============================================"
 echo "Docker Build and Sanity Check Run"
 echo "============================================"
 docker build -t emu8190 .
-echo "Expecting help output with client-ca-roots-path line:"
+echo "Expecting help output:"
 docker run -t emu8190 --help | grep "Connect your databases to multiple consumers with minimal configuration and no libraries needed"
 echo "============================================"
 echo "Docker Build and Sanity Check Passed"
